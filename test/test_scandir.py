7430f6ab2a4ecb32e00d083041263258dff71b27
Ben Hoyt <benhoyt@gmail.com>
2014-07-12 10:19:15 -0400

    Add explicit tests for .full_name attribute.

--- test/test_scandir.py
+++ test/test_scandir.py
@@ -70,3 +70,10 @@ class TestScandir(unittest.TestCase):
         self.check_file_attributes(result)
         self.assertEqual(result.st_file_attributes & scandir.FILE_ATTRIBUTE_DIRECTORY,
                          scandir.FILE_ATTRIBUTE_DIRECTORY)
+
+    def test_full_name(self):
+        entries = sorted(scandir.scandir(test_path), key=lambda e: e.name)
+        self.assertEqual([(os.path.basename(e.name), e.is_dir()) for e in entries],
+                         [('file1.txt', False), ('file2.txt', False), ('subdir', True)])
+        self.assertEqual([os.path.normpath(os.path.join(test_path, e.name)) for e in entries],
+                         [os.path.normpath(e.full_name) for e in entries])

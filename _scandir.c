956f943f03c881adec38ecde6c65260dc42778b2
brmmm3 <mrbm74@gmail.com>
2019-08-29 18:15:49 +0200

    Fix tp_flags

--- _scandir.c
+++ _scandir.c
@@ -1753,7 +1753,8 @@ static PyTypeObject ScandirIteratorType = {
     0,                                      /* tp_getattro */
     0,                                      /* tp_setattro */
     0,                                      /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                     /* tp_flags */
+    Py_TPFLAGS_DEFAULT
+        | Py_TPFLAGS_HAVE_FINALIZE,         /* tp_flags */
     0,                                      /* tp_doc */
     0,                                      /* tp_traverse */
     0,                                      /* tp_clear */
